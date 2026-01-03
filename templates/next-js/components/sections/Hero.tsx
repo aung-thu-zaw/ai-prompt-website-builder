5fee59615de423bd63288d8c3d84bff034e5fb48
aung-thu-zaw <aungthuzaw223@gmail.com>
2026-01-03 00:04:45 -0500

    add a new next js boilerplate as a website source template

---
+++ templates/next-js/components/sections/Hero.tsx
@@ -0,0 +1,25 @@
+interface HeroProps {
+  title: string;
+  description: string;
+}
+
+const Hero = ({ title, description }: HeroProps) => {
+  return (
+    <>
+      <section className="bg-white dark:bg-black">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
+              {title}
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
+              {description}
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
